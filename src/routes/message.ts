19dcdb5e8219164923b5ab8abe467d7017584cfe
Jonah Snider <jonah@jonah.pw>
2021-04-17 17:31:11 -0700

    refactor(api): stop using abbreviation for services

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -1,10 +1,10 @@
 import type { Request, Response } from 'express';
 
-import * as svcs from '../services';
+import * as services from '../services';
 import type { _, WebMessage } from '../types/common';
 
 export default function route(req: Request<_, _, WebMessage>, res: Response): void {
-  const response = svcs.voiceFlow.respond(req.body);
+  const response = services.voiceFlow.respond(req.body);
 
   res.send(response);
 }
